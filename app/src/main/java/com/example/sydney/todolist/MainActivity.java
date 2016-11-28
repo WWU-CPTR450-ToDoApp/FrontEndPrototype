f62de7e475b14dd4e2d3367ab9b89ded54a6cb7c
dschiefer95 <dschiefer95@gmail.com>
2016-11-27 18:40:08 -0800

    added settings button and page with some sample preferences that can be set and saved to use elsewhere

--- app/src/main/java/com/example/sydney/todolist/MainActivity.java
+++ app/src/main/java/com/example/sydney/todolist/MainActivity.java
@@ -3,24 +3,20 @@ package com.example.sydney.todolist;
 /* http://stackoverflow.com/questions/34579614/how-to-implement-recyclerview-in-a-fragment-with-tablayout */
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static android.R.attr.fragment;
+//import android.support.v4.app.Fragment;
 
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
@@ -31,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // Settings Button
+        Button btnOne = (Button) findViewById(R.id.settings_button);
+        btnOne.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
+                startActivity(intent);
+            }
+        });
 
         // Get the ViewPager and set it's PagerAdapter so that it can display items
         viewPager = (ViewPager) findViewById(R.id.viewpager);
@@ -57,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void addTask(View view) {
-        Fragment page = getSupportFragmentManager().findFragmentByTag("android:switcher:" + R.id.viewpager + ":" + viewPager.getCurrentItem());
+        android.support.v4.app.Fragment page = getSupportFragmentManager().findFragmentByTag("android:switcher:" + R.id.viewpager + ":" + viewPager.getCurrentItem());
         BlankFragment page2 = (BlankFragment) page;
         page2.addTask(view);
     }
@@ -98,7 +104,7 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public Fragment getItem(int position) {
+        public android.support.v4.app.Fragment getItem(int position) {
 
             switch (position) {
                 case 0:
@@ -126,4 +132,5 @@ public class MainActivity extends AppCompatActivity {
         }
 
     }
+
 }
---
+++ app/src/main/java/com/example/sydney/todolist/SettingsActivity.java
@@ -0,0 +1,37 @@
+package com.example.sydney.todolist;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.preference.SwitchPreference;
+import android.support.v7.app.AppCompatActivity;
+
+//import android.support.v4.app.Fragment;
+
+public class SettingsActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Fragment fragment = new SettingsScreen();
+        android.app.FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+        if(savedInstanceState == null) {
+            //created for the first time
+            fragmentTransaction.add(R.id.settings_layout, fragment, "settings_fragment");
+            fragmentTransaction.commit();
+        }
+        else {
+            fragment = getFragmentManager().findFragmentByTag("settings_fragment");
+        }
+    }
+
+    public static class SettingsScreen extends PreferenceFragment {
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings_screen);
+            SwitchPreference switchPreference = (SwitchPreference) findPreference("");
+        }
+    }
+}
