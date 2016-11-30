9f4b8052c3bf18549e6f230597699b955ce2e625
dschiefer95 <dschiefer95@gmail.com>
2016-11-30 09:36:40 -0800

    added settings button and page with some sample preferences that can be set and saved to use elsewhere

--- app/src/main/java/com/example/sydney/todolist/MainActivity.java
+++ app/src/main/java/com/example/sydney/todolist/MainActivity.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
@@ -141,6 +142,5 @@ public class MainActivity extends AppCompatActivity {
             tv.setText(tabTitles[position]);
             return tab;
         }
-
     }
 }
