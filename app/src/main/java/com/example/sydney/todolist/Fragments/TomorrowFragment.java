686ab2dc11752cb7318063ac0cce2bde8b812121
blakestone95 <blakestone.trumpet1@gmail.com>
2016-12-11 10:19:01 -0800

    Fixed cursors for task retrieval.

--- app/src/main/java/com/example/sydney/todolist/Fragments/TomorrowFragment.java
+++ app/src/main/java/com/example/sydney/todolist/Fragments/TomorrowFragment.java
@@ -106,15 +106,17 @@ public class TomorrowFragment extends AbstractFragment implements LoaderManager.
                         TaskContract.TaskEntry.COL_TASK_TITLE,
                         TaskContract.TaskEntry.COL_TASK_DESC
                 };
-                selection = TaskContract.TaskEntry.COL_TASK_DATE + " > ? "
-                        + "AND " + TaskContract.TaskEntry.COL_TASK_DATE + " < ?";
+                selection = TaskContract.TaskEntry.COL_TASK_DATE + " > ?"
+                        + " AND " + TaskContract.TaskEntry.COL_TASK_DATE + " <= ?"
+                        + " AND " + TaskContract.TaskEntry.COL_TASK_DONE + " = ?";
                 cal = Calendar.getInstance();
-                cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH) + 1, 0, 0, 0);
+                cal.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), 0, 0, 0);
                 calHi = Calendar.getInstance();
-                calHi.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH) + 2, 0, 0, 0);
+                calHi.set(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH) + 1, 0, 0, 0);
                 selectionArgs = new String[]{
                         String.valueOf(cal.getTimeInMillis()),
-                        String.valueOf(calHi.getTimeInMillis())};
+                        String.valueOf(calHi.getTimeInMillis()),
+                        "0"};
                 sortOrder = TaskContract.TaskEntry.COL_TASK_DATE + "," + TaskContract.TaskEntry.COL_TASK_TIME;
 
                 return new CursorLoader(getActivity(), uri, projection, selection, selectionArgs, sortOrder);
